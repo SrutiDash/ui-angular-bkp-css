6e71e07b77c4b4de04f6547bf70a41e864db2316
Sruti Dash <sruti.dash1203@gmail.com>
2024-07-01 23:28:20 +0530

    added comvivalogo on navbar by using imgbb. uploaded image in imgbb website through local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,56 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http'; 
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { LoginComponent } from './login/login.component';
+// import { UserDetailsComponent } from './user-details/user-details.component';
+// import { AllUserSearchComponent } from './all-user-search/all-user-search.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MaterialModule } from './material.module';
+// import { LoginService } from './login.service';
+// import { UserDetailsService } from './user-details.service';
+
+// import { MatTableModule } from '@angular/material/table'; 
+// import { MatFormFieldModule } from '@angular/material/form-field'; 
+// import { MatInputModule } from '@angular/material/input'; 
+// import { MatSelectModule } from '@angular/material/select'; 
+// import { MatButtonModule } from '@angular/material/button'; 
+// import { MatRadioModule } from '@angular/material/radio'; 
+// import { MatCardModule } from '@angular/material/card';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LoginComponent,
+//     UserDetailsComponent,
+//     AllUserSearchComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     ReactiveFormsModule,
+//     HttpClientModule, 
+//     MaterialModule,
+//     BrowserAnimationsModule,
+//     MatTableModule,
+//     MatFormFieldModule,
+//     MatInputModule,
+//     MatSelectModule,
+//     MatButtonModule,
+//     MatRadioModule,
+//     MatCardModule
+//   ],
+//   providers: [LoginService, UserDetailsService], 
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+
+//new
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -8,10 +61,8 @@ import { LoginComponent } from './login/login.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { AllUserSearchComponent } from './all-user-search/all-user-search.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { LoginService } from './login.service';
-import { UserDetailsService } from './user-details.service';
 
+// Angular Material Modules
 import { MatTableModule } from '@angular/material/table'; 
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { MatInputModule } from '@angular/material/input'; 
@@ -19,6 +70,10 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button'; 
 import { MatRadioModule } from '@angular/material/radio'; 
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar'; // Add this line
+
+import { LoginService } from './login.service';
+import { UserDetailsService } from './user-details.service';
 
 @NgModule({
   declarations: [
@@ -33,7 +88,6 @@ import { MatCardModule } from '@angular/material/card';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule, 
-    MaterialModule,
     BrowserAnimationsModule,
     MatTableModule,
     MatFormFieldModule,
@@ -41,7 +95,8 @@ import { MatCardModule } from '@angular/material/card';
     MatSelectModule,
     MatButtonModule,
     MatRadioModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule // Add this line
   ],
   providers: [LoginService, UserDetailsService], 
   bootstrap: [AppComponent]
